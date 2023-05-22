e1e33de6c0bd1be227d422a90f3130b826d81441
Bradley <bradleydoyle24@gmail.com>
2023-05-21 20:34:42 -0400

    Create function to play a single game
    All possible game results hard-coded in.  Prints text of the
    game winner, as well as adding to the player/computer score.

--- script.js
+++ script.js
@@ -1,32 +1,84 @@
 function getComputerChoice() {
   let number = Math.floor(Math.random() * 3) + 1;
-  console.log(number);
   if (number === 1) {
-    return "Rock";
+    return "rock";
   }
   else if (number === 2) {
-    return "Paper";
+    return "paper";
   }
   else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
 let computerChoice = getComputerChoice();
-console.log(computerChoice);
+console.log("Computer choice: " + computerChoice);
 
 function getPlayerChoice() {
   let playerChoice = prompt("Will you play 'Rock', 'Paper', or 'Scissors'?" );
   let choiceCheck = playerChoice.toLowerCase();
-  console.log(choiceCheck);
   if (choiceCheck !== 'rock' && choiceCheck !== 'scissors' && choiceCheck !== 'paper') {
     console.log("You must choose 'Rock', 'Paper', or 'Scissors'!");
     getPlayerChoice();
   } 
   else {
-    return playerChoice;
+    return choiceCheck;
   }
 }
 
 let playerChoice = getPlayerChoice();
-console.log(playerChoice);
+console.log("Player choice: " + playerChoice);
+
+function game (playerSelection, computerSelection) {
+  playerSelection = playerChoice;
+  computerSelection = computerChoice;
+
+  let computerScore = 0;
+  let playerScore = 0;
+
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
+      console.log("Rock loses to Paper, Computer Wins!");
+      computerScore += 1;
+    }
+    else if (computerSelection === 'scissors') {
+      console.log("Rock beats Scissors, You Win!");
+      playerScore += 1;
+    }
+    else {
+      console.log("Rock versus Rock, it's a Tie!");
+    }
+  }
+
+  if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      console.log("Paper beats Rock, Player Wins!");
+      playerScore += 1;
+    }
+    if (computerSelection === 'scissors') {
+      console.log("Paper loses to Scissors, Computer Wins!")
+      computerScore += 1;
+    }
+    else {
+      console.log("Paper versus Paper, it's a Tie!");
+    }
+  }
+
+  if (playerSelection === 'scissors') {
+    if (computerSelection === 'rock') {
+      console.log("Scissors loses to Rock, Computer Wins!");
+      computerScore += 1;
+    }
+    if (computerSelection === 'paper') {
+      console.log("Scissors beats Paper, Player Wins!")
+      playerScore += 1;
+    }
+    else {
+      console.log("Scissors versus Scissors, it's a Tie!");
+    }
+  }
+  console.log("Player score is : " + playerScore, " and Computer Score is: " + computerScore);
+}
+
+game(playerChoice, computerChoice);
+
