e3d8122f67057efeecc12b6b6b1fa208539c4370
Bradley <bradleydoyle24@gmail.com>
2023-06-05 23:11:45 -0400

    Copied function round() comparing playerSelection to computerSelection

--- script.js
+++ script.js
@@ -40,3 +40,50 @@ function getPlayerChoice() {
 getPlayerChoice();
 
 //Compare choices
+function round(playerSelection, computerSelection) {
+
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
+      console.log("Rock loses to Paper, Computer wins the round!");
+      computerScore += 1;
+    }
+    else if (computerSelection === 'scissors') {
+      console.log("Rock beats Scissors, You win the round!");
+      playerScore += 1;
+    }
+    else {
+      console.log("Rock versus Rock, the round is a Tie!");
+    }
+  }
+
+  else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      console.log("Paper beats Rock, Player wins the round!");
+      playerScore += 1;
+    }
+    else if (computerSelection === 'scissors') {
+      console.log("Paper loses to Scissors, Computer wins the round!")
+      computerScore += 1;
+    }
+    else {
+      console.log("Paper versus Paper, the round is a Tie!");
+    }
+  }
+
+  else if (playerSelection === 'scissors') {
+    if (computerSelection === 'rock') {
+      console.log("Scissors loses to Rock, Computer wins the round!");
+      computerScore += 1;
+    }
+    else if (computerSelection === 'paper') {
+      console.log("Scissors beats Paper, Player wins the round!")
+      playerScore += 1;
+    }
+    else {
+      console.log("Scissors versus Scissors, the round a Tie!");
+    }
+  }
+  console.log("Player score is : " + playerScore, " and Computer Score is: " + computerScore);
+
+  return playerScore, computerScore;
+}
