eaa2534a426015e9438c88b2b9db51c591ebdde9
Bradley <bradleydoyle24@gmail.com>
2023-06-07 19:12:04 -0400

    Alter function to respond to clicks in console
    Started from scratch, and created a program that responds to users
    making a selection by clicking on an icon. The program calls a
    function that returns a selection for the computer, then plays
    one round and prints the results in the console.

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-//Get computer choice
-//Get player choice by listening to what got clicked
-//Compare choices
-//Increase counter for winner
-//At 5 points, determine winner
-
-let playerScore = 0;
-let computerScore = 0;
-
-// Get computer choice
-
-// Get player choice by listening to what got clicked
-// event listener to click on certain cards
-  // Create variables for each ID in html.
-  // Function listens for click on ID
-  // Returns name in a variable 'playerChoice'
-
-
 const choices = document.querySelectorAll('.card');
 
-function getRandomChoice() {
+let playerChoice;
+let computerChoice;
+
+choices.forEach(choice => {
+  choice.addEventListener('click', ()=> {
+    playerChoice = choice.id;
+    computerChoice = getComputerChoice();
+    playSingleRound(computerChoice, playerChoice);
+  });
+});
+
+function getComputerChoice() {
   let number = Math.floor(Math.random() * 3) + 1;
   if (number === 1) {
     return "rock";
@@ -31,35 +24,23 @@ function getRandomChoice() {
   }
 }
 
-function getComputerChoice() {
-  computerChoice = getRandomChoice();
-  console.log(`computer choice function is ${computerChoice}`)
-};
-
-function getPlayerChoice() { 
-  choices.forEach(choice => {
-    choice.addEventListener('click', () => {
-      console.log(`function player choice is: ${choice.id}`);
-      return choice.id;
-    });
-  });
-}
-
-
-function round(computerChoice, playerChoice) {
+function playSingleRound(computerChoice, playerChoice) {
   if (computerChoice !== playerChoice) {
+
     if (playerChoice === 'rock') {
       if (computerChoice === 'paper') {
         console.log(`You choose ${playerChoice}, computer chooses ${computerChoice}, computer wins!`);
       } else {
         console.log(`You choose ${playerChoice}, computer chooses ${computerChoice}, player wins!`);
       }
+
     } else if (playerChoice === 'paper') {
       if (computerChoice === 'scissors') {
         console.log(`You choose ${playerChoice}, computer chooses ${computerChoice}, computer wins!`);
       } else {
         console.log(`You choose ${playerChoice}, computer chooses ${computerChoice}, player wins!`);
       }
+
     } else if (playerChoice === 'scissors') {
         if (computerChoice === 'rock') {
         console.log(`You choose ${playerChoice}, computer chooses ${computerChoice}, computer wins!`);
@@ -72,14 +53,3 @@ function round(computerChoice, playerChoice) {
     console.log(`${playerChoice} vs ${computerChoice}, it\'s a tie!`);
   }
 }
-
-
-choices.forEach(choice => {
-  choice.addEventListener('click', () => {
-    let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
-    round(computerChoice, playerChoice);
-  });
-});
-
-//Compare choices
